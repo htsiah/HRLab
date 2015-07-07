8bab55e25b07b3ea858261eb0179860dfa01b908
htsiah <htsiah@hotmail.com>
2015-07-07 18:13:21 +0800

    CR P2: Default Error Handling Page

--- app/Global.scala
+++ app/Global.scala
@@ -1,5 +1,6 @@
 import play.api._
 import play.api.mvc._
+import play.api.mvc.Results._
 import play.api.data._
 import play.api.libs.concurrent.Execution.Implicits._
 import play.api.libs.concurrent.Akka
@@ -8,11 +9,33 @@ import akka.actor.Actor
 import akka.actor.Props
 
 import scala.concurrent.duration._
+import scala.concurrent.Future
 import jobs._
+import views._
 
 object Global extends GlobalSettings {
+  
+  // 400 - bad request
+  override def onBadRequest(request: RequestHeader, error: String) = {
+    Future.successful(BadRequest("Bad Request: " + error))
+  } 
+  
+  // 500 - internal server error
+  override def onError(request: RequestHeader, throwable: Throwable) = {
+    Future.successful(InternalServerError(
+        html.error.onerror(throwable.getMessage())
+    ))
+  }
+  
+  // 404 - page not found error
+  override def onHandlerNotFound(request: RequestHeader) = {
+    Future.successful(NotFound(
+        html.error.onhandlernotfound()  
+    ))  
+  }
+    
   override def onStart(app: Application) {
-     
+    
     // Parameter 1: initial delat
     // Parameter 2: interval
     Akka.system(app).scheduler.schedule(1 seconds, 1 seconds) {
@@ -26,6 +49,6 @@ object Global extends GlobalSettings {
     Akka.system(app).scheduler.schedule(1 seconds, 4 hours) {
       MonthlyLeaveProfileUpdateJob.run
     }
-      	
+          	
   }
 }
