159e3b6b25588ec6f59a30a09d0ac1d97c9e454e
htsiah <htsiah@hotmail.com>
2016-03-11 17:15:36 +0800

    Leave File Model and Controller done.

---
+++ app/controllers/LeaveFileController.scala
@@ -0,0 +1,101 @@
+package controllers
+
+import models.LeaveFileModel
+
+import javax.inject.Inject
+
+import play.api._
+import play.api.mvc._
+import play.modules.reactivemongo.{
+  MongoController, ReactiveMongoApi, ReactiveMongoComponents
+}
+
+import play.api.libs.json.{ Json, JsObject, JsString }
+import play.modules.reactivemongo.json._
+
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+
+import reactivemongo.api.gridfs.ReadFile
+import reactivemongo.bson._
+
+import org.joda.time.DateTime
+
+import scala.concurrent.Future
+
+class LeaveFileController @Inject() (val reactiveMongoApi: ReactiveMongoApi) extends Controller with MongoController with ReactiveMongoComponents with Secured {
+  
+  import MongoController.readFileReads
+  type JSONReadFile = ReadFile[JSONSerializationPack.type, JsString]
+
+  def insert(p_lk: String) = withAuth (parse.maxLength(1 * 1014 * 1024, gridFSBodyParser(LeaveFileModel.gridFS))) { username => implicit request => {
+    
+    request.body match {
+      case Left(MaxSizeExceeded(length)) => {  
+                
+        // Return error
+        Future(Ok(Json.obj("status" -> "exceed file size limit")).as("application/json"))
+            
+      }
+      case Right(multipartForm) => {
+        
+        // here is the future file!
+        val futureFile = request.body.right.get.files.head.ref
+        
+        // Return error on file upload
+        futureFile.onFailure {
+          case err => {
+            err.printStackTrace();
+            Ok(Json.obj("status" -> "error upload file")).as("application/json")
+          }
+        }
+        
+        // when the upload is complete, we add the some metadata
+        val futureUpdate = for {
+          file <- futureFile
+          
+          // here, the file is completely uploaded, so it is time to update the article
+          updateResult <- {        
+            LeaveFileModel.gridFS.files.update(
+                Json.obj("_id" -> file.id),
+                Json.obj("$set" -> Json.obj("folder" -> "supporting-doc", "metadata" -> Json.obj("lk" -> p_lk, "mdat" -> new DateTime(), "mby" -> request.session.get("username"))))
+            )
+          }
+          
+        } yield updateResult
+        
+        // Return success
+        futureUpdate.map { _ =>
+          Ok(Json.obj("status" -> "success")).as("application/json")
+        }.recover {
+          case err => {
+            err.printStackTrace();
+            Ok(Json.obj("status" -> "error update metadata")).as("application/json")
+          }
+        }
+      }
+    }
+            
+  }}
+  
+  def view(p_id: String) = Action.async { request => {
+    
+    // find the matching attachment, if any, and streams it to the client
+    val file = LeaveFileModel.gridFS.find[JsObject, JSONReadFile](Json.obj("_id" -> p_id))
+    serve[JsString, JSONReadFile](LeaveFileModel.gridFS)(file)
+    
+  }}
+
+  def delete(p_id:String) = withAuth { username => implicit request => {
+    
+    LeaveFileModel.gridFS.remove(Json toJson p_id).map(_ => 
+      Ok(Json.obj("status" -> "success")).as("application/json")
+    ).recover {
+      case err => {
+        err.printStackTrace();
+        Ok(Json.obj("status" -> "error")).as("application/json")
+      }
+    }
+    
+  }}
+
+}
---
+++ app/models/LeaveFileModel.scala
@@ -0,0 +1,35 @@
+package models
+
+import play.api.Play
+import scala.util.Try
+
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import play.api.Logger
+import play.modules.reactivemongo.json._
+
+import reactivemongo.api._
+import reactivemongo.api.gridfs.GridFS
+
+object LeaveFileModel {
+  
+  private val dbname = Play.current.configuration.getString("mongodb_leave_file").getOrElse("leave_file")
+  private val uri = Play.current.configuration.getString("mongodb_leave_file_uri").getOrElse("mongodb://localhost")
+  private val driver = new MongoDriver()
+  private val connection: Try[MongoConnection] = MongoConnection.parseURI(uri).map { 
+    parsedUri => driver.connection(parsedUri)
+  }
+  private val db = connection.get.db(dbname)
+  val gridFS = this.getGridFS
+
+  // let's build an index on our gridfs chunks collection if none
+  gridFS.ensureIndex().onComplete {
+    case index =>
+      Logger.info(s"Checked index, result is $index")
+  }
+  
+  private def getGridFS = {
+    import play.modules.reactivemongo.json.collection._
+    GridFS[JSONSerializationPack.type](db)
+  }
+
+}
