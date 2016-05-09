0338fb4cf1ee25c28fe531721f3eac714ce6f774
htsiah <htsiah@hotmail.com>
2016-05-09 15:58:27 +0800

    CR V3: Add My Leave Balance on Dashboard [Leave]

--- app/controllers/DashboardController.scala
+++ app/controllers/DashboardController.scala
@@ -2,7 +2,7 @@ package controllers
 
 import play.api.mvc._
 import play.api.libs.concurrent.Execution.Implicits._
-import models.{LeaveModel}
+import models.{LeaveModel,LeaveProfileModel}
 import scala.concurrent.Future
 import reactivemongo.api._
 import reactivemongo.bson.{BSONObjectID,BSONDocument}
@@ -14,8 +14,9 @@ class DashboardController extends Controller with Secured {
     for {
       leaves <- LeaveModel.find(BSONDocument("pid"->request.session.get("id").get, "wf.s"->"Pending Approval", "ld" -> false), BSONDocument("docnum" -> -1), request)
       tasks <- TaskModel.find(BSONDocument("pid"->request.session.get("id").get, "cf"->false), BSONDocument("sys.cdat" -> -1), request)
+      leaveprofiles <- LeaveProfileModel.find(BSONDocument("pid"->request.session.get("id").get), BSONDocument("lt" -> 1), request)
     } yield {
-      Ok(views.html.dashboard.index(tasks, leaves)).withSession(
+      Ok(views.html.dashboard.index(tasks, leaves, leaveprofiles)).withSession(
           (request.session - "path") + ("path"->((routes.DashboardController.index).toString))
       )
     }
