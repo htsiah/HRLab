a5b21d48f260f7f47b65db5f9fb5353637ae56d1
htsiah <htsiah@hotmail.com>
2016-07-28 11:45:58 +0800

    Bugfix: Leave Profile in Report does not display properly.

--- public/javascripts/leaveprofilereport.form.js
+++ public/javascripts/leaveprofilereport.form.js
@@ -23,10 +23,22 @@ $(function(){
 		btn_down_class:'btn-danger'
 	});
 
-	$('#e1,#e2,#e3,#e4,#e5,#e1_cf,#e2_cf,#e3_cf,#e4_cf,#e5_cf').ace_spinner({
+	$('#e1,#e2,#e3,#e4,#e5,#e6,#e7,#e8,#e9,#e10').ace_spinner({
 		value:0,
 		min:0,
-		max:100,
+		max:120,
+		step:1, 
+		on_sides: true, 
+		icon_up:'ace-icon fa fa-plus smaller-75', 
+		icon_down:'ace-icon fa fa-minus smaller-75', 
+		btn_up_class:'btn-success' , 
+		btn_down_class:'btn-danger'
+	});
+	
+	$('#e1_cf,#e2_cf,#e3_cf,#e4_cf,#e5_cf,#e6_cf,#e7_cf,#e8_cf,#e9_cf,#e10_cf').ace_spinner({
+		value:0,
+		min:0,
+		max:120,
 		step:1, 
 		on_sides: true, 
 		icon_up:'ace-icon fa fa-plus smaller-75', 
@@ -35,7 +47,7 @@ $(function(){
 		btn_down_class:'btn-danger'
 	});
 
-	$('#e1_s,#e2_s,#e3_s,#e4_s,#e5_s').ace_spinner({
+	$('#e1_s,#e2_s,#e3_s,#e4_s,#e5_s,#e6_s,#e7_s,#e8_s,#e9_s,#e10_s').ace_spinner({
 		value:0,
 		min:0,
 		max:999,
